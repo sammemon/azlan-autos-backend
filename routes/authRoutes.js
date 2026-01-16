c06c495fc37d216cf67fe10234648dcbe27036ac
SohailAhmed25 <thehappinesses0@gmail.com>
2026-01-17 01:29:58 +0500

    Remove Google signup route to fix deployment error

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -13,7 +13,6 @@ const {
   signup,
   verifyEmail,
   resendVerification,
-  googleSignUp,
 } = require('../controllers/authController');
 
 router.post(
@@ -76,17 +75,5 @@ router.put(
 
 router.get('/users', protect, authorize('admin'), getAllUsers);
 router.put('/users/:id/status', protect, authorize('admin'), updateUserStatus);
-
-router.post(
-  '/google-signup',
-  [
-    body('email').isEmail().withMessage('Valid email is required'),
-    body('name').notEmpty().withMessage('Name is required'),
-    body('phone').notEmpty().withMessage('Phone is required'),
-    body('idToken').notEmpty().withMessage('Google ID token is required'),
-  ],
-  validateRequest,
-  googleSignUp
-);
 
 module.exports = router;
